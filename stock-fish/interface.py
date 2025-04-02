bfb12ffb5c727ad4f021cba1c95df17c4fca0b73
MerlynG <c.playstore.cp@gmail.com>
2025-04-02 16:07:55 +0200

    python stockfish interface working

--- stock-fish/interface.py
+++ stock-fish/interface.py
@@ -35,10 +35,78 @@ q = queue.Queue()
 t = threading.Thread(target=enqueue_output, args=(p.stdout, q))
 t.daemon = True
 t.start()
-print(write_and_read(p, q, 'uci', 'uciok').split('\n')[-1])
+uciok = write_and_read(p, q, 'uci', 'uciok').split('\n')[-1]
+if uciok != 'uciok':
+    print('uci not ok')
+    sys.exit()
+
+try: sys.argv[1]
+except: 
+    print("""Usage : interface.py <option>
+        
+Options :
+    new          - Create a new game in starting position
+    rm <n>       - Cancel last n moves
+    moves <list> - Add the list to the moves done
+    go <n>       - AI play the best move with n level of prediction (default 10)
+""")
+    sys.exit()
 
 if sys.argv[1] == 'new':
-    
+    write_and_read(p, q, 'position startpos', stop_keyword=-1)
+    with open('track_pos.txt', 'w') as f:
+        f.write('position startpos')
+    with open('track_pos.txt', 'r') as f:
+        print(f.read())
+
+elif sys.argv[1] == 'rm':
+    with open('track_pos.txt', 'r') as f:
+        pos = f.read()
+    if len(pos.split()) == 2:
+        print('nothing to remove')
+        sys.exit()
+    pos = pos.split()
+    with open('track_pos.txt', 'w') as f:
+        if int(sys.argv[2]) != 0:
+            f.write(' '.join(pos[0:0-int(sys.argv[2])]))
+        else:
+            f.write(' '.join(pos))
+    with open('track_pos.txt', 'r') as f:
+        print(f.read())
+
+elif sys.argv[1] == 'moves':
+    with open('track_pos.txt', 'r') as f:
+        pos = f.read()
+    with open('track_pos.txt', 'a') as f:
+        if len(pos.split()) == 2:
+            f.write(' moves')
+        for m in sys.argv[2:]:
+            f.write(' ' + m)
+    with open('track_pos.txt', 'r') as f:
+        print(f.read())
+
+elif sys.argv[1] == 'go':
+    with open('track_pos.txt', 'r') as f:
+        pos = f.read()
+    write_and_read(p, q, pos, -1)
+    try:
+        depth = sys.argv[2]
+    except:
+        depth = '10'
+    best = write_and_read(p, q, 'go depth ' + depth)
+    with open('track_pos.txt', 'a') as f:
+        f.write(' ' + best.split('\n')[-1].split()[1])
+    print(' ' + best.split('\n')[-1].split()[1])
+
+else:
+    print("""Usage : interface.py <option>
+        
+Options :
+    new          - Create a new game in starting position
+    rm <n>       - Cancel last n moves
+    moves <list> - Add the list to the moves done
+    go <n>       - AI play the best move with n level of prediction (default 10)
+""")
 
 # print(write_and_read(p, q, 'position startpos'))
 # print(write_and_read(p, q, 'go depth 15'))
