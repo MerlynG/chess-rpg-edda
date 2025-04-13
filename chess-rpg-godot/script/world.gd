0f437f54cf0340859830be0bf26ceeb8877ce804
MerlynG <c.playstore.cp@gmail.com>
2025-04-13 14:24:21 +0200

    Add assets and start world building

---
+++ chess-rpg-godot/script/world.gd
@@ -0,0 +1,188 @@
+extends TileMapLayer
+@onready var character_body_2d: CharacterBody2D = $"../Allies/CharacterBody2D"
+
+@onready var allies: Node2D = $"../Allies"
+@onready var enemies: Node2D = $"../Enemies"
+@onready var area_limit: Area2D = $"../Limits/AreaLimit"
+@onready var camera_2d: Camera2D = $"../Camera2D"
+
+@export var cam_target: Node2D
+
+const ENEMY = preload("res://scene/enemy.tscn")
+const PLAYER = preload("res://scene/player.tscn")
+const tile_size = 32
+const max_moves = 8
+
+var turn = true
+var possible_2_steps_pos: Array[Vector2]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	character_body_2d.change_sprite("wn")
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if cam_target:
+		camera_2d.global_position = cam_target.global_position
+	for a in allies.get_children():
+		for e in enemies.get_children():
+			if positions_equal(a.global_position, e.global_position):
+				if !turn:
+					print(e.get_texture(), " captured by ", a.get_texture())
+					enemies.remove_child(e)
+				else:
+					print(a.get_texture(), " captured by ", e.get_texture())
+					allies.remove_child(a)
+	if !turn:
+		#For now it works like this but that shit need a fix because wtf
+		turn = true
+		await get_tree().create_timer(0.5).timeout
+		#var e = enemies.get_child(-1)
+		#e._move_to(Vector2(e.global_position.x, e.global_position.y+tile_size))
+
+func is_allie(piece: CharacterBody2D):
+	var texture = piece.get_texture()
+	return texture.begins_with("w")
+
+func get_moves(piece: CharacterBody2D, piece_type: String, dir: Vector2):
+	var moves: Array[Vector2]
+	var pos = piece.global_position
+	match piece_type:
+		"p":
+			var diag_gauche = pos + tile_size * (dir + dir.rotated(-PI/2))
+			var diag_droite = pos + tile_size * (dir + dir.rotated(PI/2))
+			var is_front_free = true
+			if pos in possible_2_steps_pos: moves.append(pos + tile_size * dir * 2)
+			for e in enemies.get_children():
+				if positions_equal(e.global_position, pos + tile_size * dir):
+					is_front_free = false
+					continue
+				if positions_equal(e.global_position, diag_droite):
+					moves.append(diag_droite)
+					continue
+				if positions_equal(e.global_position, diag_gauche):
+					moves.append(diag_gauche)
+					continue
+			if is_front_free: moves.append(pos + tile_size * dir)
+			for i in range(moves.size()):
+				if is_off_limit(moves[i], area_limit):
+					moves.remove_at(i)
+					continue
+				for a in allies.get_children():
+					if positions_equal(moves[i], a.global_position):
+						moves.remove_at(i)
+						break
+			return moves
+		"r":
+			var dirs = [Vector2(0, 1), Vector2(1, 0), Vector2(0, -1), Vector2(-1, 0)]
+			var all_pieces = allies.get_children() + enemies.get_children()
+			for d in dirs:
+				var i = 1
+				while i < max_moves:
+					var found_piece = false
+					var temp = pos + tile_size * i * d
+					if is_off_limit(temp, area_limit): break
+					for p in all_pieces:
+						if positions_equal(temp, p.global_position):
+							found_piece = true
+							if !is_allie(p):
+								moves.append(temp)
+							break
+					if found_piece: break
+					else: moves.append(temp)
+					i += 1
+			return moves
+		"n":
+			var dirs = [Vector2(0, 1), Vector2(1, 0), Vector2(0, -1), Vector2(-1, 0)]
+			for d in dirs:
+				var t1 = pos + tile_size * 2 * d + tile_size * d.rotated(PI/2)
+				var t2 = pos + tile_size * 2 * d + tile_size * d.rotated(-PI/2)
+				if !is_off_limit(t1, area_limit):
+					var ally_on_target = false
+					for a in allies.get_children():
+						if positions_equal(a.global_position, t1): ally_on_target = true
+					if !ally_on_target: moves.append(t1)
+				if !is_off_limit(t2, area_limit):
+					var ally_on_target = false
+					for a in allies.get_children():
+						if positions_equal(a.global_position, t2): ally_on_target = true
+					if !ally_on_target: moves.append(t2)
+			return moves
+		"b":
+			var dirs = [Vector2(1, 1), Vector2(1, -1), Vector2(-1, -1), Vector2(-1, 1)]
+			var all_pieces = allies.get_children() + enemies.get_children()
+			for d in dirs:
+				var i = 1
+				while i < max_moves:
+					var found_piece = false
+					var temp = pos + tile_size * i * d
+					if is_off_limit(temp, area_limit): break
+					for p in all_pieces:
+						if positions_equal(temp, p.global_position):
+							found_piece = true
+							if !is_allie(p):
+								moves.append(temp)
+							break
+					if found_piece: break
+					else: moves.append(temp)
+					i += 1
+			return moves
+		"q":
+			var dirs = [Vector2(1, 1), Vector2(1, -1), Vector2(-1, -1), Vector2(-1, 1), Vector2(0, 1), Vector2(1, 0), Vector2(0, -1), Vector2(-1, 0)]
+			var all_pieces = allies.get_children() + enemies.get_children()
+			for d in dirs:
+				var i = 1
+				while i < max_moves:
+					var found_piece = false
+					var temp = pos + tile_size * i * d
+					if is_off_limit(temp, area_limit): break
+					for p in all_pieces:
+						if positions_equal(temp, p.global_position):
+							found_piece = true
+							if !is_allie(p):
+								moves.append(temp)
+							break
+					if found_piece: break
+					else: moves.append(temp)
+					i += 1
+			return moves
+		"k":
+			var dirs = [Vector2(1, 1), Vector2(1, -1), Vector2(-1, -1), Vector2(-1, 1), Vector2(0, 1), Vector2(1, 0), Vector2(0, -1), Vector2(-1, 0)]
+			var all_pieces = allies.get_children() + enemies.get_children()
+			for d in dirs:
+				var found_piece = false
+				var temp = pos + tile_size * d
+				if is_off_limit(temp, area_limit): continue
+				for p in all_pieces:
+					if positions_equal(temp, p.global_position):
+						found_piece = true
+						if !is_allie(p):
+							moves.append(temp)
+						continue
+				if found_piece: continue
+				else: moves.append(temp)
+			return moves
+	return []
+
+func uci_to_vect(uci: String):
+	var x = uci[0].to_upper().unicode_at(0) - 'A'.unicode_at(0)
+	return Vector2(x * tile_size + 16, (8 - int(uci[1])) * tile_size + 10)
+
+func vect_to_uci(vect: Vector2):
+	return char(97 + ((vect[0] - 16) / tile_size)) + str(8 - int((vect[1] - 10) / tile_size))
+
+func is_off_limit(point: Vector2, area: Area2D) -> bool:
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = point
+	query.collide_with_areas = true
+	var result = space_state.intersect_point(query)
+	
+	for item in result:
+		if item.collider == area:
+			return true
+	return false
+
+func positions_equal(a: Vector2, b: Vector2, epsilon := 0.01) -> bool:
+	return a.distance_to(b) < epsilon
