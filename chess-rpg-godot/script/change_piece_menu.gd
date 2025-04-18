cb5a2c7a4e719af87a23304ad8fc1dfccd9fd062
MerlynG <c.playstore.cp@gmail.com>
2025-04-19 01:11:07 +0200

    Add hover animation on piece change menu

--- chess-rpg-godot/script/change_piece_menu.gd
+++ chess-rpg-godot/script/change_piece_menu.gd
@@ -2,20 +2,34 @@ extends Control
 
 @export var player: CharacterBody2D
 
-@onready var pawn: TextureButton = $VBoxContainer/Pawn
-@onready var rook: TextureButton = $VBoxContainer/Rook
-@onready var knight: TextureButton = $VBoxContainer/Knight
-@onready var bishop: TextureButton = $VBoxContainer/Bishop
-@onready var queen: TextureButton = $VBoxContainer/Queen
-@onready var king: TextureButton = $VBoxContainer/King
+@onready var pawn: TextureButton = $VBoxContainer/Control/Pawn
+@onready var rook: TextureButton = $VBoxContainer/Control2/Rook
+@onready var knight: TextureButton = $VBoxContainer/Control4/Knight
+@onready var bishop: TextureButton = $VBoxContainer/Control3/Bishop
+@onready var queen: TextureButton = $VBoxContainer/Control5/Queen
+@onready var king: TextureButton = $VBoxContainer/Control6/King
+
+@onready var control_pawn: Control = $VBoxContainer/Control
+@onready var control_rook: Control = $VBoxContainer/Control2
+@onready var control_knight: Control = $VBoxContainer/Control4
+@onready var control_bishop: Control = $VBoxContainer/Control3
+@onready var control_queen: Control = $VBoxContainer/Control5
+@onready var control_king: Control = $VBoxContainer/Control6
+
+@onready var texture_rect_pawn: TextureRect = $VBoxContainer/Control/TextureRect_Pawn
+@onready var texture_rect_rook: TextureRect = $VBoxContainer/Control2/TextureRect_Rook
+@onready var texture_rect_bishop: TextureRect = $VBoxContainer/Control3/TextureRect_Bishop
+@onready var texture_rect_knight: TextureRect = $VBoxContainer/Control4/TextureRect_Knight
+@onready var texture_rect_queen: TextureRect = $VBoxContainer/Control5/TextureRect_Queen
+@onready var texture_rect_king: TextureRect = $VBoxContainer/Control6/TextureRect_King
 
 func _ready() -> void:
-	pawn.visible = true
-	rook.visible = false
-	knight.visible = false
-	bishop.visible = false
-	queen.visible = false
-	king.visible = false
+	control_pawn.visible = true
+	control_rook.visible = false
+	control_knight.visible = false
+	control_bishop.visible = false
+	control_queen.visible = false
+	control_king.visible = false
 	match player.get_texture():
 		"wp": pawn.button_pressed = true
 		"wr": rook.button_pressed = true
@@ -68,9 +82,9 @@ func _process(_delta: float):
 			bishop.disabled = false
 			queen.disabled = false
 			king.disabled = true
-	if GameState.puzzle1_success: rook.visible = true
-	if GameState.puzzle2_success: bishop.visible = true
-	if GameState.puzzle3_success: knight.visible = true
+	if GameState.puzzle1_success: control_rook.visible = true
+	if GameState.puzzle2_success: control_bishop.visible = true
+	if GameState.puzzle3_success: control_knight.visible = true
 
 func _on_pawn_button_down() -> void:
 	player.change_sprite("wp")
@@ -119,3 +133,40 @@ func _on_king_button_down() -> void:
 	knight.button_pressed = false
 	bishop.button_pressed = false
 	queen.button_pressed = false
+
+
+func _on_pawn_mouse_entered() -> void:
+	texture_rect_pawn.visible = true
+
+func _on_pawn_mouse_exited() -> void:
+	texture_rect_pawn.visible = false
+
+func _on_rook_mouse_entered() -> void:
+	texture_rect_rook.visible = true
+
+func _on_rook_mouse_exited() -> void:
+	texture_rect_rook.visible = false
+
+func _on_bishop_mouse_entered() -> void:
+	texture_rect_bishop.visible = true
+
+func _on_bishop_mouse_exited() -> void:
+	texture_rect_bishop.visible = false
+
+func _on_knight_mouse_entered() -> void:
+	texture_rect_knight.visible = true
+
+func _on_knight_mouse_exited() -> void:
+	texture_rect_knight.visible = false
+
+func _on_queen_mouse_entered() -> void:
+	texture_rect_queen.visible = true
+
+func _on_queen_mouse_exited() -> void:
+	texture_rect_queen.visible = false
+
+func _on_king_mouse_entered() -> void:
+	texture_rect_king.visible = true
+
+func _on_king_mouse_exited() -> void:
+	texture_rect_king.visible = false
