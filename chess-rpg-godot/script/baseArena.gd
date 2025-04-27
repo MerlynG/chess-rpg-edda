6a715f8e630d319d610660381cc96d671b5e0991
MerlynG <c.playstore.cp@gmail.com>
2025-04-27 15:19:53 +0200

    Test Linux output

--- chess-rpg-godot/script/baseArena.gd
+++ chess-rpg-godot/script/baseArena.gd
@@ -61,7 +61,7 @@ func _process(_delta: float) -> void:
 		external_process_node.SendInput("position startpos" + moves)
 		external_process_node.SendInput("go depth 1")
 		var res = external_process_node.ReadAllAvailableOutput("bestmove")
-		print(res)
+		print([res.split("\n")[-2]])
 		#var fen = StockfishConnector.pos_to_fen(allies.get_children(), enemies.get_children())
 		#StockfishConnector.pers(fen)
 		#var m = StockfishConnector.go(1)
